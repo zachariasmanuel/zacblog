caf0a010345796ed720788e6c8e448bd6939742b
Zacharias Manuel <zacflys@gmail.com>
2018-12-19 12:37:48 +0530

    Extract email blog post

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push -f origin master
-git subtree push --force --prefix=public git@github.com:zachariasmanuel/zacblog.git gh-pages 
+git subtree push -f --prefix=public git@github.com:zachariasmanuel/zacblog.git gh-pages 
 
 echo -e "\033[0;32mDone..\033[0m"
